657e5c240d4f747af323f135caf679c6512bc5c0
Crina Berinde <cberinde@sdl.com>
2021-01-18 12:51:22 +0200

    error handling meddleware

--- config/errorHandling.js
+++ config/errorHandling.js
@@ -7,5 +7,7 @@ module.exports = {
 function errorHandling(app) {
   app.use(function (err, req, res, next) {
     console.log("err ", err.message);
+
+    return res.status(err.statusCode || 400).json(err);
   });
 }
